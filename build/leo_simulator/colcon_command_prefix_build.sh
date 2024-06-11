6532f1db97f009ceb738158498678f71c9bbe5b7
Kelian Perez <kelianperez42@outlook.fr>
2024-06-11 16:27:03 +0200

    Revert "Lidar + rgbd SLAM is working"
    This reverts commit 528177150738f9caaff3e21694f226fb5114e06e.

--- build/leo_simulator/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ism_student_2023/ros2_ws/src/leo_simulator-ros2/install/leo_gz_plugins/share/leo_gz_plugins/package.sh"
-. "/home/ism_student_2023/ros2_ws/src/leo_simulator-ros2/install/leo_gz_worlds/share/leo_gz_worlds/package.sh"
-. "/home/ism_student_2023/ros2_ws/src/leo_simulator-ros2/install/leo_gz_bringup/share/leo_gz_bringup/package.sh"
