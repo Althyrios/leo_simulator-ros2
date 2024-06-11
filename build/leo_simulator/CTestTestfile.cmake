528177150738f9caaff3e21694f226fb5114e06e
Kelian Perez <kelianperez42@outlook.fr>
2024-06-11 16:05:50 +0200

    Lidar + rgbd SLAM is working

---
+++ build/leo_simulator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ism_student_2023/ros2_ws/src/leo_simulator-ros2/leo_simulator
+# Build directory: /home/ism_student_2023/ros2_ws/src/leo_simulator-ros2/build/leo_simulator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
